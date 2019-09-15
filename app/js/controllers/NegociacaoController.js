0b41cf74321d7688450cd4d4c49e10519c651f9c
Rafael <rafaelwaterkemper@gmail.com>
2019-09-15 20:04:51 -0300

    Adicionado a classe NegociacoesView

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -1,12 +1,20 @@
 class NegociacaoController {
     constructor() {
+        this.negociacoes = new Negociacoes();
+        this.negociacaoView = new NegociacoesView("#negociacoesView");
         this.data = document.querySelector("#data");
         this.quantidade = document.querySelector("#quantidade");
         this.valor = document.querySelector("#valor");
+        this.negociacaoView.update();
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(this.data.valueAsDate, this.quantidade.valueAsNumber, this.valor.valueAsNumber);
-        console.log(negociacao);
+        const negociacao = new Negociacao(this.data.valueAsDate, parseInt(this.quantidade.value), parseFloat(this.valor.value));
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        });
     }
 }
--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -3,14 +3,17 @@ class NegociacaoController {
     private data: HTMLInputElement;
     private quantidade: HTMLInputElement;
     private valor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacaoView = new NegociacoesView("#negociacoesView");
 
     constructor() {
         this.data = <HTMLInputElement>document.querySelector("#data");
         this.quantidade = <HTMLInputElement>document.querySelector("#quantidade");
         this.valor = <HTMLInputElement>document.querySelector("#valor");
+        this.negociacaoView.update();
     }
 
-    adiciona(event: Event) {
+    adiciona(event: Event): void {
         
         event.preventDefault();
         
@@ -20,6 +23,11 @@ class NegociacaoController {
             parseFloat(this.valor.value)
         )
 
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.paraArray().forEach(negociacao => {
+            console.log(negociacao.data);
+            console.log(negociacao.quantidade);
+            console.log(negociacao.valor);
+        });
     }
 }
